47810039215fe06c9adf8cdab9307960833a3a31
riki karasawa <fabulous.riki@gmail.com>
2021-12-29 18:15:04 +0900

    再利用できる関数コンポーネントを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import defaultDataset from './dataset';
-import './assets/styles/style.css'
+import './assets/styles/style.css';
+import {AnswersList} from './components/index';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,12 +15,26 @@ export default class App extends React.Component {
     };
   }
 
+  // 初期表示データセット関数
+  initAnswer = () => {
+    const initDataset = this.state.dataset[this.state.currentId]
+    const initAnswers = initDataset.answers
+
+    this.setState({
+      answers: initAnswers
+    })
+  }
+
+  componentDidMount() {
+    this.initAnswer()
+  }
+
   render() {
     return (
       <div>
         <section className="c-section">
           <div className="c-box">
-            {this.state.currentId}
+            <AnswersList answers={this.state.answers} />
           </div>
         </section>
       </div>
---
+++ src/components/Answer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import {makeStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+// Material UI 読み込み
+// const useStyles = makeStyles(() => ({
+//     root: {
+
+//     }
+// }))
+
+const Answer = (props) => {
+    // const classes = useStyles();
+
+    return (
+        <Button variant="contained" color="primary">
+            {props.content}
+        </Button>
+    )
+}
+
+export default Answer
